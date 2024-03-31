f1030d63396b690e58a738c28052f39c9def5230
DorijanKomsic <110539593+DorijanKomsic@users.noreply.github.com>
2024-03-31 22:04:32 +0200

    Added file reader and basic flag arguments

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
+)
 
-func main()  {
-   fmt.Println("Hello World!") 
+var imagePath string
+
+func init() {
+    const (
+        defaultPath = ""
+        usage = "Path to image to be converted"
+    )
+
+    flag.StringVar(&imagePath, "image", defaultPath, usage)
+    flag.StringVar(&imagePath, "i", defaultPath, usage+" (shorthand)")
+}
+
+func main() {
+   flag.Parse()
+   _, err := getImageFromPath(imagePath)
+   if err != nil {
+       log.Fatal(err)
+   }
+   fmt.Println("Image returned" ) 
+}
+
+
+func getImageFromPath(filePath string) (image.Image, error)  {
+    f, err := os.Open(filePath) 
+    defer f.Close()
+    if err != nil {
+        return nil, err
+    }
+    image, _, err := image.Decode(f)
+    return image, err
 }
